9c972615e87fe7b5311d0f9e0da2534eba31a1df
Abin Thomas <abinthomas909@gmail.com>
2024-08-22 11:20:33 +0530

    first commit

---
+++ components3/Nexhome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Nexhome = () => {
+  return (
+    <div style={{ textAlign: 'right', marginRight: '450px' }}>
+        <h1>Depth, Dimension, Discovery</h1>
+        <h1>:The Nexus Effect</h1>
+    </div>
+  )
+}
+
+export default Nexhome
